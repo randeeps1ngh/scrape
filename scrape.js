dd7b92ee495a909ac05aacdfe712754680be0fb7
marcburnett <marc_burnett@yahoo.com>
2019-03-29 09:34:04 +0000

    Add files via upload

---
+++ scrape.js
@@ -0,0 +1,34 @@
+//dependencies
+const request = require('request');
+const cheerio = require('cheerio');
+
+//searching pages 1 to 5
+for (let i=1; i<=5; i++){
+
+   const URL = `https://workinstartups.com/job-board/jobs/interns/?p=${i}`
+
+    //querying URL
+   request(URL, (error, response, html) => {
+    if (error){
+       
+        //error messages
+        console.log('It did not work');
+        console.log(response.statusCode + error);
+        console.log(error);
+        
+    }
+   
+    //tracking the pages as they are scraped
+    //still puzzled how they are not scraped in order; might be the 'Asynchronous' thing
+    console.log('Page '+i);
+
+        //loading the html 
+        const $ = cheerio.load(html);
+
+        //scraping using the selector and outputting content
+        const first = $('a.job-link');
+        console.log(first.text());
+      
+    });
+
+}
